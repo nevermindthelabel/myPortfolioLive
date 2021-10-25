012e6ab71ac78b2bc08775fae774192b35917981
Matt Kilcup <nevermindthelabel@gmail.com>
2021-10-24 21:33:42 -0700

    hid anayltics on localhost and updated js

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,5 @@
 const toggle = document.querySelector('.theme-switch');
+const checkbox = document.getElementById('theme-switer-checkbox');
 const main = document.querySelector('main');
 const nav = document.querySelector('nav');
 const footer = document.querySelector('footer');
@@ -9,11 +10,23 @@ function switchTheme() {
   main.classList.toggle('dark');
   nav.classList.toggle('dark');
   footer.classList.toggle('dark');
+  checkbox.hasAttribute('checked') ? checkbox.removeAttribute('checked') : checkbox.setAttribute('checked', 'checked');
 }
 
 function load() {
+  checkUserMode();
   document.getElementsByClassName('align-middle')[0].appendChild(document.createTextNode(new Date().getFullYear()));
 }
+
+const checkUserMode = () => {
+  if (window.matchMedia) {
+    // Check if the dark-mode Media-Query matches
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      switchTheme();
+    }
+  }
+};
+
 window.onload = load;
 
 toggle.addEventListener('change', switchTheme, false);
